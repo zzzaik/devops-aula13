dbf0d555361ecbe52af08ef12e7e78d1880c6300
Gustavo Zaik Freitas <zzzaik21@gmail.com>
2017-11-08 20:41:29 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -85,7 +85,7 @@ def lista_candidatos(request):
         context_instance = RequestContext(request,
         {
             'title':'Cadastro de candidatos',
-            'candidatos': Candidatos.objects.all(),
+            'candidatos': Candidato.objects.all(),
             'year':datetime.now().year,
         })
     )
