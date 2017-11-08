f7b746f541c4d20dbf7fbf68bfb287fd07f177c0
Tomaz <tmsski@yahoo.com.br>
2017-11-08 16:31:26 -0200

    versão inicial

---
+++ app/models.py
@@ -0,0 +1,15 @@
+"""
+Definition of models.
+"""
+
+from django.db import models
+
+# Create your models here.
+class Curso(models.Model):
+    nome = models.CharField(max_length=200)
+    periodo = models.CharField(max_length=50)
+    instituicao = models.CharField(max_length=200)
+
+class Vestibular(models.Model):
+    nome = models.CharField(max_length=200)
+
